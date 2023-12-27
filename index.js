7c0cf300229342b9a7f35e156da759c663dec37a
Sivadhas <sivadhas007@gmail.com>
2023-12-27 22:03:29 +0530

    initial setup

---
+++ index.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
