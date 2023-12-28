77faca8058c517f146896c7e3c405cd727b42c52
Sivadhas <sivadhas007@gmail.com>
2023-12-28 11:21:07 +0530

    feat(handler) modified index.js to handle the command

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 require('dotenv').config()
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands=new Collection();
+
+// as command grows , do this dynamically
+let command=require('./commands/trashme')
+client.commands.set(command.data.name,command);
+
 
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
